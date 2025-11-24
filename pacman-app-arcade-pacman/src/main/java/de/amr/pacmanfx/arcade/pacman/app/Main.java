44112eafd1002a480699306a4c8483644c5bd4d9
Lucas Andrade <57198329+AndradeHd@users.noreply.github.com>
2025-11-23 23:41:24 -0300

    Delete pacman-app-arcade-pacman directory

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/app/Main.java
+++
@@ -1,9 +0,0 @@
-package de.amr.pacmanfx.arcade.pacman.app;
-
-import de.amr.pacmanfx.uilib.Ufx;
-
-public class Main {
-    public static void main(String[] args) {
-        Ufx.startApplication(ArcadePacMan_App.class);
-    }
-}
